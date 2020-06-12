be846506fae15c1bae4b5e997aa2ef288caa283d
Kjartan Oli Agustsson <kjartanoli@protonmail.com>
2020-06-12 23:46:50 +0000

    Added License info

--- src/chapter.cpp
+++ src/chapter.cpp
@@ -1,5 +1,23 @@
+/*
+ * Copyright (C) 2020  Ágústsson, Kjartan Óli <kjartanoli@protonmail.com>
+ * Author: Ágústsson, Kjartan Óli <kjartanoli@protonmail.com>
+ *
+ * This file is a part of epubmaker
+ * epubmaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * epubmaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <fstream>
-#include <limits>
 #include <string_view>
 #include <string>
 #include <vector>
@@ -7,7 +25,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <regex>
-#include <limits>
 
 #include "../headers/chapter.hpp"
 #include "../headers/misc.hpp"
--- src/navPoint.cpp
+++ src/navPoint.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2020  Ágústsson, Kjartan Óli <kjartanoli@protonmail.com>
+ * Author: Ágústsson, Kjartan Óli <kjartanoli@protonmail.com>
+ *
+ * This file is a part of epubmaker
+ * epubmaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * epubmaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string>
 #include <string_view>
 
@@ -14,6 +33,9 @@ NavPoint::NavPoint(std::string_view source, std::string_view text)
 
 NavPoint::operator std::string() const
 {
-	return "<navPoint id=\"" + this->id + "\" playOrder=\"" + std::to_string(this->playOrder) + "\">\n\t\t\t<navLabel><text>" + this->text + "</text></navLabel>\n\t\t\t<content src=\"Text/" + this->source + "\"/>\n\t\t</navPoint>";
+	return "<navPoint id=\"" + this->id + "\" playOrder=\"" +
+		std::to_string(this->playOrder) + "\">\n\t\t\t<navLabel><text>" +
+		this->text + "</text></navLabel>\n\t\t\t<content src=\"Text/" +
+		this->source + "\"/>\n\t\t</navPoint>";
 }
 
