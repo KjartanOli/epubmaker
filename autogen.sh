ba5f91d95abee066ceda40f74bfaa33721a23ff0
Kjartan Óli Ágústsson <kjartanoli@protonmail.com>
2021-03-01 11:30:42 +0000

    Allow autogen.sh to bypass automake's insistence on having no extensions on required files

--- autogen.sh
+++ autogen.sh
@@ -1,1 +1,8 @@
+# automake does not accept any extension on the required files, but GitLab doesn't recognise them as a Org files without the .org extension
+mv README.org README
+mv NEWS.org NEWS
+
 autoreconf --install || exit 1
+
+mv README README.org
+mv NEWS NEWS.org
